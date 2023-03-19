4eade169883ebd9588e1a0f87075d15fe2d3391e
Sumegh20 <sumegh20@gmail.com>
2023-03-19 23:48:30 +0530

    add home in route "/"

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ os.makedirs("uploaded_file", exist_ok = True)
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/')
+def home():
+    return 'Code Working'
+
 @app.route('/file-upload', methods=['GET'])
 def upload_file():
 	# check if the post request has the file part
